10af2ccf4b04249631c5c22b3f3af33278de0eea
diegofernandoalayon <dfar.41@gmail.com>
2021-09-08 12:20:44 -0500

    creando custom hook para estraer los estados del formulario

---
+++ src/components/SearchForm/hook.js
@@ -0,0 +1,57 @@
+import { useReducer } from "react"
+
+
+const ACTIONS = {
+    UPDATE_KEYWORD: 'keyword',
+    UPTADE_RATING: 'rating',
+    UPDATE_LIMIT: 'limit'
+}
+const reducer =(state, action) =>{
+    
+    switch (action.type) {
+        case ACTIONS.UPDATE_KEYWORD:
+            return{
+                ...state,
+                keyword: action.payload,
+                times: state.times + 1
+            }
+        case ACTIONS.UPTADE_RATING:
+            return{
+                ...state,
+                rating:action.payload
+            }
+        case ACTIONS.UPDATE_LIMIT:
+            return{
+                ...state,
+                limit: action.payload
+            }
+        default:
+            return state
+
+    }
+}
+export default function useForm  ({initialKeyword,initialRating,initialLimit}) {
+    const [state, dispatch] = useReducer(reducer,{
+        keyword:decodeURI(initialKeyword),
+        rating:initialRating,
+        limit:initialLimit,
+        times: 0}) // useReducer retorna el estado y un metodo dispatch, y recibe, un reducer y un initialState
+    
+    const {keyword, rating, times,limit} = state
+    return {
+        keyword,
+        rating,
+        times,
+        limit,
+        dispatch,
+        updateKeyword: (keyword) =>
+            dispatch({type: ACTIONS.UPDATE_KEYWORD,payload:keyword}),
+        updateRating: (rating) => 
+            dispatch({type: ACTIONS.UPTADE_RATING,payload:rating}),
+        updateLimit: (limit) =>
+            dispatch({type: ACTIONS.UPDATE_LIMIT,payload:limit})
+           
+    }
+}
+
+
--- src/components/SearchForm/index.js
+++ src/components/SearchForm/index.js
@@ -1,66 +1,28 @@
-import React, {useState,useReducer} from 'react'
+import React from 'react'
 import {useLocation} from 'wouter'
+import useForm from './hook'
 
 const RATINGS = ['g','pg','pg-13','r']
 const LIMIT = ['5','10','15','20'] 
-const ACTIONS = {
-    UPDATE_KEYWORD: 'keyword',
-    UPTADE_RATING: 'rating',
-    UPDATE_LIMIT: 'limit'
-}
-const reducer =(state, action) =>{
-    console.log(action)
-    switch (action.type) {
-        case ACTIONS.UPDATE_KEYWORD:
-            return{
-                ...state,
-                keyword: action.payload,
-                times: state.times + 1
-            }
-        case ACTIONS.UPTADE_RATING:
-            return{
-                ...state,
-                rating:action.payload
-            }
-        case ACTIONS.UPDATE_LIMIT:
-            return{
-                ...state,
-                limit: action.payload
-            }
-        default:
-            return state
-
-    }
-}
 
 function SearchForm({initialKeyword = '',initialRating='g',initialLimit='10'}){
   
-    // const [limit, setLimit] = useState(initialLimit)
-    // const [rating, setRating] = useState(initialRating)
-    const [state, dispatch] = useReducer(reducer,{
-        keyword:decodeURI(initialKeyword),
-        rating: initialRating,
-        limit: 10,
-        times: 0}) // useReducer retorna el estado y un metodo dispatch, y recibe, un reducer y un initialState
-    const {keyword, rating,times,limit} = state
+    const {keyword, rating,times,limit,updateKeyword,updateRating, updateLimit} = useForm({initialKeyword,initialRating,initialLimit})
     const [path, pushLocation] = useLocation()
     const handleSubmit = event =>{ //lo que se debe hacer al momento de enviar
         event.preventDefault()
         // navegar a otra ruta
         pushLocation(`/search/${keyword}/${limit}/${rating}`)
-        // onSubmit({keyword,limit})
     }
  
     const handleChange = event =>{ //lo que acuerre con cada cambio en el texto
-        // setKeyword(event.target.value) //actualizamos la keyword con cada letra que se ponga
-        dispatch({type: ACTIONS.UPDATE_KEYWORD,payload:event.target.value})
+        updateKeyword(event.target.value)      
     }
     const handleLimit = event =>{
-        dispatch({type: ACTIONS.UPDATE_LIMIT,payload:event.target.value}) 
-       
+        updateLimit(event.target.value) 
     }
     const handleChangeRating = (event)=>{
-        dispatch({type: ACTIONS.UPTADE_RATING,payload:event.target.value})
+        updateRating(event.target.value) 
     }
     return(
         <form onSubmit={handleSubmit} className='search'>
