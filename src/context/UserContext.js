dc649fc0da945dd00ec78c5fe5e8ca9d6fc520ac
diegofernandoalayon <dfar.41@gmail.com>
2021-09-15 12:16:34 -0500

    agregando servicio para adquirir los favoritos desde que se carga la pagina

--- src/context/UserContext.js
+++ src/context/UserContext.js
@@ -1,4 +1,5 @@
-import React,{useState} from 'react'
+import React,{useState,useEffect} from 'react'
+import getFavs from '../services/getFavs'
 
 const Context = React.createContext({})
 
@@ -6,6 +7,12 @@ export function UserContextProvider({children}){
     const [favs,setFavs] = useState([])
     const [jwt, setJWT] = useState(()=>window.sessionStorage.getItem('jwt')) //jwt -> javaScript web token// se puede poner una funcion para poner estado inicial, ya que esta funcion se ejecuta una vez
 
+    useEffect(()=>{ //efecto para que cada que cambie un usuario, busque si este usuario tiene favoritos, 
+        if(!jwt) return setFavs([])
+        getFavs({jwt}).then(setFavs)
+
+
+    },[jwt])
     //de esta forma podemos usar el context para tenerlo como global, y usar el estado del context 
     return <Context.Provider value={{
         favs,
