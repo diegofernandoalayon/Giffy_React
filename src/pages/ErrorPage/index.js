47e715b4e04435de641c918d5fbdf38e419ec465
diegofernandoalayon <dfar.41@gmail.com>
2021-09-18 15:23:35 -0500

    uso de emotion/css

--- src/pages/ErrorPage/index.js
+++ src/pages/ErrorPage/index.js
@@ -1,19 +1,57 @@
-import { Link } from "wouter"
-import './ErrorPage.css'
 
-const gifsError = ['KlrMS4vyq5KSY','8L0Pky6C83SzkzU55a','oYQ9HRm5Mo7VXeMNVR','3osxY9kuM2NGUfvThe','haZOqHKz9tTfW','mq5y2jHRCAqMo']
-export default function ErrorPage(){
+import { Link } from "wouter";
+// import "./ErrorPage.css";
+import { css,jsx } from "@emotion/css";
+import { Helmet } from "react-helmet";
 
-    const randomImage = () =>{
-        const random = Math.floor(Math.random()*gifsError.length) + 1
-        return `https://i.giphy.com/media/${gifsError[random]}/giphy.webp`
-    }
-    
-    return (
-    <div>
-    <img src={randomImage()} alt='error image'/>
-    <Link to="/"><button className='error-btn'>Home</button></Link>
+const gifsError = [
+    "KlrMS4vyq5KSY",
+    "8L0Pky6C83SzkzU55a",
+    "oYQ9HRm5Mo7VXeMNVR",
+    "3osxY9kuM2NGUfvThe",
+    "haZOqHKz9tTfW",
+    "mq5y2jHRCAqMo",
+];
+const containt = css`
+width: 40%;
+height: 100vh;
+@media screen and (max-width: 55rem){
+    width: 90%;
+}
+`
+const errorBtn = css`
+width: 80%;
+background-color: rgba(127, 225, 250, 0.829);
+border-radius: 5px;
+border: none;
+margin-top: 20px;
+font-size: 24px;
+&:hover{
+    background-color: rgba(16, 207, 255, 0.829);
+    color: black;
+}
+`
+const img = css`
+width: 80%;
+`
+
+
+export default function ErrorPage() {
+  const randomImage = () => {
+    const random = Math.floor(Math.random() * gifsError.length) + 1;
+    return `https://i.giphy.com/media/${gifsError[random]}/giphy.webp`;
+  };
+
+  return (
+    <div className={containt}>
+        <Helmet>
+            <title> Error 404 | Giffy</title>
+        </Helmet>
+      <h2>Error 404</h2>
+      <img src={randomImage()} className={img} alt="error image" />
+      <Link to="/">
+        <button className={errorBtn}>Home</button>
+      </Link>
     </div>
-        )
-
+  );
 }
